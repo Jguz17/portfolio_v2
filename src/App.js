afdc8f2e615fdd210f18647db7afc2caf14b8c48
jguz17 <johnathanguzman17@gmail.com>
2020-12-20 23:37:00 -0600

    Initial files. First section is setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div className="App" >
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import FirstSection from './components/FirstSection'
 
-    </div>
-  );
+const App = () => {
+  return (
+    <Grid container>
+      <Grid item md={2}/>
+      <Grid container item md={8}>
+        <Grid container item xs={12} id="first-section-container">
+          <FirstSection/>
+        </Grid>
+      </Grid>
+      <Grid item md={2}/>
+    </Grid>
+  )
 }
 
-export default App;
+export default App
