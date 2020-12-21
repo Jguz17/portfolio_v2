bb39f7792af41957321f5323fbf1de3e5468c402
jguz17 <johnathanguzman17@gmail.com>
2020-12-21 17:22:04 -0600

    Added most content and styles for about me section. Need to make side nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid';
 import FirstSection from './components/FirstSection'
+import SecondSection from './components/SecondSection'
+import Arrow from './components/layout/Arrow'
+import Nav from './components/layout/Nav'
 
 const App = () => {
   return (
     <Grid container>
       <Grid item md={2}/>
       <Grid container item md={8}>
-        <Grid container item xs={12} id="first-section-container">
-          <FirstSection/>
+        <Grid container item xs={12} id="section-container">
+          {/* <FirstSection/> */}
+          <SecondSection/>
+          <Arrow/>
+          {/* <Nav/> */}
         </Grid>
       </Grid>
       <Grid item md={2}/>
