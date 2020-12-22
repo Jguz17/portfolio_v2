0aa409a3a8482af8d677b13bd70546e21baba99c
jguz17 <johnathanguzman17@gmail.com>
2020-12-21 22:11:05 -0600

    Added hover effect on icons

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -4,11 +4,11 @@ import { Home, HelpCircle, Layers, Briefcase, Share2 } from 'react-feather';
 const Nav = () => {
 
     const desktop = <div className="desktop-nav">
-        <Home stroke-width=".5" width="75" height="75"/>
-        <HelpCircle stroke-width=".5" width="75" height="75"/>
-        <Layers stroke-width=".5" width="75" height="75"/>
-        <Briefcase stroke-width=".5" width="75" height="75"/>
-        <Share2 stroke-width=".5" width="75" height="75"/>
+        <Home class="icon" stroke-width=".5" width="75" height="75"/>
+        <HelpCircle class="icon" stroke-width=".5" width="75" height="75"/>
+        <Layers class="icon" stroke-width=".5" width="75" height="75"/>
+        <Briefcase class="icon" stroke-width=".5" width="75" height="75"/>
+        <Share2 class="icon" stroke-width=".5" width="75" height="75"/>
     </div>
 
     const mobile = <div className="mobile-nav"><Home/></div>
