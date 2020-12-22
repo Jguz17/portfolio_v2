83a7b3a18c7c8f20a9b3bf186e093d0eb6700809
jguz17 <johnathanguzman17@gmail.com>
2020-12-21 21:57:36 -0600

    Created desktop nav bar

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
           {/* <FirstSection/> */}
           <SecondSection/>
           <Arrow/>
-          {/* <Nav/> */}
+          <Nav/>
         </Grid>
       </Grid>
       <Grid item md={2}/>
--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Home, HelpCircle, Layers, Briefcase, Share2 } from 'react-feather';
 
 const Nav = () => {
+
+    const desktop = <div className="desktop-nav">
+        <Home stroke-width=".5" width="75" height="75"/>
+        <HelpCircle stroke-width=".5" width="75" height="75"/>
+        <Layers stroke-width=".5" width="75" height="75"/>
+        <Briefcase stroke-width=".5" width="75" height="75"/>
+        <Share2 stroke-width=".5" width="75" height="75"/>
+    </div>
+
+    const mobile = <div className="mobile-nav"><Home/></div>
+
     return (
-        <div>
-            <p>Nav</p>
+        <div className="nav-container">
+            {desktop}
         </div>
     )
 }
